a0e2a82bd9b6e28e0e5e69184983eab15703529c
mdradityatama <mdimasradityatama@gmail.com>
2019-08-28 17:32:57 +0700

    Support Markdown

--- config/app.php
+++ config/app.php
@@ -120,7 +120,7 @@ return [
     */
 
     'providers' => [
-
+        GrahamCampbell\Markdown\MarkdownServiceProvider::class,
         /*
          * Laravel Framework Service Providers...
          */
@@ -174,7 +174,7 @@ return [
     */
 
     'aliases' => [
-
+        'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
         'App' => Illuminate\Support\Facades\App::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
