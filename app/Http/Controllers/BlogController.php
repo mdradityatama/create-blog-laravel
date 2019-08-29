0f796eaa9a3e767affd1ed333f6ed6aaf7e3a2c4
mdradityatama <mdimasradityatama@gmail.com>
2019-08-29 13:20:22 +0700

    filter daftar tulisan berdasarkan ketegori

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Category;
 
 class BlogController extends Controller
 {
@@ -11,6 +12,7 @@ class BlogController extends Controller
 
     public function index()
     {
+      // dd($categories);
       // \DB::enableQueryLog();
       $posts = Post::with('author')
                     ->LatestFirst()
@@ -18,6 +20,22 @@ class BlogController extends Controller
                     ->simplePaginate($this->limit);
       // dd($posts);
       return view("blog.index", compact('posts'));
+      // dd(\DB::getQueryLog());
+    }
+
+    public function category(Category $category)
+    {
+      $categoryName = $category->title;
+
+      // dd($categories);
+      // \DB::enableQueryLog();
+      $posts = $category->posts()
+                        ->with('author')
+                        ->LatestFirst()
+                        ->published()
+                        ->simplePaginate($this->limit);
+      // dd($posts);
+      return view("blog.index", compact('posts', 'categoryName'));
       // dd(\DB::getQueryLog());
     }
 
